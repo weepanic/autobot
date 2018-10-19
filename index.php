b08a1e3a98c21fad7f37851b5f0d959c5d69d29a
weepanic <32123277+weepanic@users.noreply.github.com>
2018-10-19 13:55:28 +0700

    Update index.php

--- index.php
+++ index.php
@@ -23,19 +23,25 @@ if (!is_null($events['events'])) {
         // Line API send a lot of event type, we interested in message only.
         if ($event['type'] == 'message') {
             switch($event['message']['type']) {
-                case 'text':
             
                 // Get replyToken
                 $replyToken = $event['replyToken'];
 
-                // Reply message
-                $respMessage = 'Hello, your message is '. $event['message']['text'];
-                $respMessage = 'Hello, your message is '. $event['message']['text']; 
-                $httpClient = new CurlHTTPClient($channel_token); 
-                $bot = new LINEBot($httpClient, array('channelSecret' => $channel_secret)); 
-                $textMessageBuilder = new TextMessageBuilder($respMessage); 
-                $response = $bot->replyMessage($replyToken, $textMessageBuilder);
+
+                switch($event['message']['type']) {
+                case 'image':
+                $messageID = $event['message']['id'];
+                $respMessage = 'Hello, your image ID is '. $messageID;
                 break;
+
+                default:
+                $respMessage = 'Please send image only';
+                break;
+
+                $httpClient = new CurlHTTPClient($channel_token);
+                $bot = new LINEBot($httpClient, array('channelSecret' => $channel_secret));
+                $textMessageBuilder = new TextMessageBuilder($respMessage);
+                $response = $bot->replyMessage($replyToken, $textMessageBuilder);
             } 
         } 
     } 
